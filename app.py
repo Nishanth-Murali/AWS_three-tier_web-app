9ff966f77c3581ef759af6fb98a9f8df673b33d3
Nishanth Murali <64389100+Nishanth-Murali@users.noreply.github.com>
2021-08-06 14:24:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,121 +35,115 @@ app.config['UPLOAD_FOLDER'] = '/home/ubuntu/flaskproject/uploads'
 # Allowed extension you can set your own
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-# s3_resource = boto3.resource('s3')
-# s3_client = boto3.client('s3')
-# sqs_resource = boto3.resource('sqs')
-# sqs_client = boto3.client('sqs')
-# ec2_resource = boto3.resource('ec2')
-# send_queue_name = 'send_queue.fifo'
-# receive_queue_name = 'receive_queue.fifo'
-# session = boto3.session.Session()
+s3_resource = boto3.resource('s3')
+s3_client = boto3.client('s3')
+sqs_resource = boto3.resource('sqs')
+sqs_client = boto3.client('sqs')
+ec2_resource = boto3.resource('ec2')
+send_queue_name = 'send_queue.fifo'
+receive_queue_name = 'receive_queue.fifo'
+session = boto3.session.Session()
 
 
-# def create_bucket(bucket_name, s3_connection):
+ def create_bucket(bucket_name, s3_connection):
     
-#     #current_region = session.region_name
+     current_region = session.region_name
     
-#     s3_connection.create_bucket(
-#         Bucket=bucket_name)
-#         #CreateBucketConfiguration={
-#         #'LocationConstraint': current_region})
-#     #print(bucket_name, current_region)
-#     return bucket_name
+     s3_connection.create_bucket(
+         Bucket=bucket_name)
+         CreateBucketConfiguration={
+         'LocationConstraint': current_region})
+     print(bucket_name, current_region)
+     return bucket_name
 
-# def upload_file_to_s3(filepath, bucket_name, key):
-#         try:
-#             s3_resource.meta.client.upload_file(filepath, bucket_name, key)
-#             logger.info("File has been uploaded successfully to %s",bucket_name)
-#         except Exception as error:
-#             logger.error("Error occurred while uploading file to s3 bucket %s", bucket_name)
-#             raise error
+ def upload_file_to_s3(filepath, bucket_name, key):
+         try:
+             s3_resource.meta.client.upload_file(filepath, bucket_name, key)
+             logger.info("File has been uploaded successfully to %s",bucket_name)
+         except Exception as error:
+             logger.error("Error occurred while uploading file to s3 bucket %s", bucket_name)
+             raise error
 
-# def send_message(messageBody, queue_name):
-#         try:
-#             queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
-#         except Exception as error:
-#             logger.warning("Queue doesnot exist %s", error)
-#             # create_sqs(queue_name)
-#             # queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
+ def send_message(messageBody, queue_name):
+         try:
+             queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
+         except Exception as error:
+             logger.warning("Queue doesnot exist %s", error)
+             create_sqs(queue_name)
+             queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
 
 
-#         response = queue.send_message(MessageBody=messageBody, MessageGroupId='string')
-#         if 'Successful' in response:
-#             for msg_meta in response['Successful']:
-#                 '''logger.info(
-#                     "Message sent: %s: %s",
-#                     msg_meta['MessageId'],
-#                     messages[int(msg_meta['Id'])]['body']
-#                 )'''
-#         if 'Failed' in response:
-#             for msg_meta in response['Failed']:
-#                 '''logger.warning(
-#                     "Failed to send: %s: %s",
-#                     msg_meta['MessageId'],
-#                     messages[int(msg_meta['Id'])]['body']
-#                 )'''
-#         return response
-
+         response = queue.send_message(MessageBody=messageBody, MessageGroupId='string')
+         if 'Successful' in response:
+             for msg_meta in response['Successful']:
+                 '''logger.info(
+                     "Message sent: %s: %s",
+                     msg_meta['MessageId'],
+                     messages[int(msg_meta['Id'])]['body']
+                 )'''
+         if 'Failed' in response:
+             for msg_meta in response['Failed']:
+                 '''logger.warning(
+                     "Failed to send: %s: %s",
+                     msg_meta['MessageId'],
+                     messages[int(msg_meta['Id'])]['body']
+                 )'''
+         return response
 # def create_sqs(queue_name):
-#         try:
-#             response = sqs_resource.create_queue(QueueName=queue_name, Attributes={'FifoQueue': 'true','ContentBasedDeduplication':'true'})
-#             logger.warning("Queue has been created with url %s", response.url)
-#         except Exception as error:
-#             logger.exception("Couldn't create queue")
-#             raise error
-#         else:
-#             return response
-
+         try:
+             response = sqs_resource.create_queue(QueueName=queue_name, Attributes={'FifoQueue': 'true','ContentBasedDeduplication':'true'})
+             logger.warning("Queue has been created with url %s", response.url)
+         except Exception as error:
+             logger.exception("Couldn't create queue")
+             raise error
+         else:
+             return response
 # def count_running_instances():
-#         try:
-#             instances = ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-#             instances_list = [instance for instance in instances]
-	    
-#         except Exception as error:
-#             logger.error("Error occured while counting number of instances")
-#             raise error
-#         return len(instances_list)
-
+         try:
+             instances = ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+             instances_list = [instance for instance in instances]
+    
+         except Exception as error:
+             logger.error("Error occured while counting number of instances")
+             raise error
+         return len(instances_list)
 # def create_instance(image_id):
-#         try:
-#             ec2_resource.create_instances(ImageId=image_id, MinCount=1, MaxCount=1,
-#                                  InstanceType='t2.micro')
-#             logger.info("Instance created with imageId %s",image_id)
-#         except Exception as error:
-#             logger.warning("Error occurred while creating instance")
-#             raise error
-
-# def get_length_of_queue(queue_name):
-#         try:
-#             sqs_queue = sqs_resource.get_queue_by_name(QueueName = queue_name)
-#             messages = sqs_queue.attributes["ApproximateNumberOfMessages"]
-#         except Exception as error:
-#             logger.warning("Error occurred while getting length of the queue!")
-#             raise error
-#         return int(messages)
-
-# def receive_message(queue_name):
-#         try:
-#             queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
-#             full_message = None
-#             for message in queue.receive_messages(WaitTimeSeconds=20):
-#                 full_message = message
-#                 break
-#         except Exception as error:
-#             logger.exception("Unable to receive message from queue : %s", queue_name)
-#             raise error
-#         else:
-#             return full_message
-
-# def delete_message(queue_url, receipt_handle):
-#         try:
-#             sqs_client.delete_message(
-#                 QueueUrl=queue_url,
-#                 ReceiptHandle=receipt_handle
-#             )
-#             logger.info("Message %s has been deleted from the queue %s",receipt_handle,queue_url)
-#         except Exception as error:
-#             logger.exception()
+         try:
+             ec2_resource.create_instances(ImageId=image_id, MinCount=1, MaxCount=1,
+                                  InstanceType='t2.micro')
+             logger.info("Instance created with imageId %s",image_id)
+         except Exception as error:
+             logger.warning("Error occurred while creating instance")
+             raise error
+ def get_length_of_queue(queue_name):
+         try:
+             sqs_queue = sqs_resource.get_queue_by_name(QueueName = queue_name)
+             messages = sqs_queue.attributes["ApproximateNumberOfMessages"]
+         except Exception as error:
+             logger.warning("Error occurred while getting length of the queue!")
+             raise error
+         return int(messages)
+ def receive_message(queue_name):
+         try:
+             queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
+             full_message = None
+             for message in queue.receive_messages(WaitTimeSeconds=20):
+                 full_message = message
+                 break
+         except Exception as error:
+             logger.exception("Unable to receive message from queue : %s", queue_name)
+             raise error
+         else:
+             return full_message
+ def delete_message(queue_url, receipt_handle):
+         try:
+             sqs_client.delete_message(
+                 QueueUrl=queue_url,
+                 ReceiptHandle=receipt_handle
+             )
+             logger.info("Message %s has been deleted from the queue %s",receipt_handle,queue_url)
+         except Exception as error:
+             logger.exception()
 #             raise error
 
 
